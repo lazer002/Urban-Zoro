6f23a05ce178dba182d7d5df3b2fa6a0c068f42c
ajitmanthan <ajit.kumar@manthanitsolutions.in>
2024-04-01 17:56:49 +0530

    a

--- db/mongo.js
+++ db/mongo.js
@@ -1,5 +1,6 @@
 const mongo = require("mongoose")
-const mon = 'mongodb+srv://lazerxd002:fakedatabase@cluster0.1xzl4xe.mongodb.net/urbunzoro'
+
+const mon = 'mongodb+srv://ajit_manthan:ajitmanthan@cluster0.fvikk66.mongodb.net/no?retryWrites=true&w=majority&appName=Cluster0'
 mongo.connect(mon,{
 }).then(()=>{
     console.log('db connected');
